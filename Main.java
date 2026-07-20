438b474c3ab8194cd21d861e57dd7f376ccdad5b
Jordan <jordiw255621@gmail.com>
2026-07-20 11:45:55 +1000

    Shared a draw.io file.

--- Main.java
+++ Main.java
@@ -9,12 +9,13 @@ class SeatingView {
   public void printSeating() {
     for (int i = 0; i < 10; i++) {
       for (int j = 0; j < 10; j++) {
-        System.out.print(seats[i][j] + " ") ;
+        System.out.print(seats[i][j] + " ");
       }
       System.out.println();
     }
   }
 }
+
 class Main {
   public static void main(String[] args) {
     SeatingView seating = new SeatingView();
