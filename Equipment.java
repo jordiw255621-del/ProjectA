2936b8c5c1a129e6f646a2bec0be0caf141ac358
Jordan <jordiw255621@gmail.com>
2026-07-21 22:52:36 +1000

    Constructed the Equipment and Person classes

--- Equipment.java
+++ Equipment.java
@@ -1,16 +1,55 @@
-class Equipment{
+class Equipment implements costable {
     private String name;
     private EquipmentType equipmentType;
+    private double hireCost; // cost per gear
 
+    // just uses whatever the default price is for that type of gear
+    Equipment(String name, EquipmentType equipmentType) {
+        this.name = name;
+        this.equipmentType = equipmentType;
+        this.hireCost = equipmentType.getDefaultPrice();
+    }
 
-    Equipment(){
+    // Overloading the constructor so we can set our own custom price, eg, the
+    // artist wants different gear
+    Equipment(String name, EquipmentType equipmentType, double hireCost) {
+        this.name = name;
+        this.equipmentType = equipmentType;
+        this.hireCost = hireCost;
 
     }
 
     public String getName() {
         return this.name;
     }
+
+    public EquipmentType getEquipmentType() {
+        return this.equipmentType;
+    }
+
+    // need this cause we implement costable, booking uses it to add up total cost
+    public double getPrice() {
+        return this.hireCost;
+    }
+
+    // Still need a toString() method for the GUI
+
 }
 
+// list of equipment that artists can choose for their set
+enum EquipmentType {
+    DRUM_KIT(150.00),
+    PIANO(300.00),
+    SPEAKER(80.00),
+    MICROPHONE(25.00);
 
-enum EquipmentType{}
+    private final double defaultPrice;
+
+    EquipmentType(double defaultPrice) {
+        this.defaultPrice = defaultPrice;
+    }
+
+    public double getDefaultPrice() {
+        return this.defaultPrice;
+    }
+}
--- Person.java
+++ Person.java
@@ -1,5 +1,85 @@
-abstract class Person{}
+import java.util.ArrayList;
+import java.util.List;
 
-class Fan extends Person{}
+// the parent class that branches out to Fan and Artist classes
+abstract class Person {
+    private String name;
+    private String email;
 
-class Artist extends Person{}
+    Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    // every person has a role but only the child classes actually know what it is
+    public abstract String getRole();
+
+    public String toString() {
+        return getRole() + ": " + this.name + " (" + this.email + ")";
+    }
+}
+
+// the fan is just a normal user who buys/reserves tickets to shows
+class Fan extends Person {
+    private List<Ticket> purchasedTickets;
+
+    Fan(String name, String email) {
+        super(name, email);
+        this.purchasedTickets = new ArrayList<>();
+    }
+
+    public void addTicket(Ticket ticket) {
+        this.purchasedTickets.add(ticket);
+    }
+
+    public List<Ticket> getPurchasedTickets() {
+        return this.purchasedTickets;
+    }
+
+    @Override
+    public String getRole() {
+        return "Fan";
+    }
+}
+
+// the artist is who actually books the slot + picks their gear for the show
+class Artist extends Person {
+    private String genre;
+    private List<Equipment> requestedEquipment;
+
+    // no args constructor so Booking can make a placeholder artist for now
+    Artist() {
+        this("Unknown", "unknown@gmail.com", "Unspecified");
+    }
+
+    Artist(String name, String email, String genre) {
+        super(name, email);
+        this.genre = genre;
+        this.requestedEquipment = new ArrayList<>();
+    }
+
+    public void addEquipment(Equipment item) {
+        this.requestedEquipment.add(item);
+    }
+
+    public List<Equipment> getRequestedEquipment() {
+        return this.requestedEquipment;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    @Override
+    public String getRole() {
+        return "Artist";
+    }
+}
