c00a6f47c7fff24986eca86f97b8653628e4ce86
Daniel <daniel.lukins1@gmail.com>
2026-07-21 18:43:15 +1000

    create base classes Menu and Main

--- Main.java
+++ Main.java
@@ -1,31 +1,10 @@
-
-class SeatingView {
-  int[][] seats;
-
-  SeatingView() {
-    seats = new int[10][10];
-    seats[3][6] = 1;
-  }
-
-  public void printSeating() {
-    for (int i = 0; i < 10; i++) {
-      for (int j = 0; j < 10; j++) {
-        System.out.print(seats[i][j] + " ");
-      }
-      System.out.println();
-    }
-  }
-}
+import java.util.List;
 
 class Main {
-  public static void main(String[] args) {
-    SeatingView seating = new SeatingView();
-    seating.printSeating();
-  }
+  List<Booking> bookings;
 
 }
 
 class Menu {
 
-  // menu a menu b methods etc.
 }
