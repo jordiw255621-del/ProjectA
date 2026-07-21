4501a23687255ed13320aac178bddc4ec2eca07c
Daniel <daniel.lukins1@gmail.com>
2026-07-21 19:18:52 +1000

    added rowcount + columncount static ints

--- Equipment.java
+++ Equipment.java
@@ -1,3 +1,16 @@
-class Equipment{}
+class Equipment{
+    private String name;
+    private EquipmentType equipmentType;
+
+
+    Equipment(){
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
+
 
 enum EquipmentType{}
--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 import java.util.List;
 
 class Main {
+
+  // represents how many rows and columns for the seats
+  static final int ROW_COUNT = 10;
+  static final int COLUMN_COUNT = 10;
   List<Booking> bookings;
 
 }
