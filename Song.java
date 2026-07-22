753c7343c7c7dbd9ba972df655331738eef4e41f
Daniel <daniel.lukins1@gmail.com>
2026-07-22 20:11:37 +1000

    added getters for artist + name

--- Song.java
+++ Song.java
@@ -1,13 +1,22 @@
 public class Song {
    private String name;
-   private Artist artist; 
+   private Artist artist;
 
    Song(String name, Artist artist) {
-    this.name = name;
-    this.artist = artist;
+      this.name = name;
+      this.artist = artist;
    }
 
    Song(String name, String artistName) {
 
    }
+
+   public String getName() {
+      return this.name;
+   }
+
+   public Artist getArtist() {
+      return this.artist;
+   }
+
 }
