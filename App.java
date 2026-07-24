6f32c01b4f2cbdff0319aceb1e87bee8d96c3e94
Daniel <daniel.lukins1@gmail.com>
2026-07-24 11:11:29 +1000

    added the App file to be run

---
+++ App.java
@@ -0,0 +1,17 @@
+// The actual app file to be ran
+
+public class App {
+    public static void main(String[] args) {
+        Main main = new Main();
+        Artist artist1 = new Artist("bob", "bob@gmail.com", "pop");
+        Booking booking1 = new Booking(artist1, 10);
+        booking1.addSong(new Song("coolsong", artist1));
+        booking1.addSong(new Song("awesomesong", artist1));
+        booking1.addSong(new Song("badsong", artist1));
+        booking1.printSongProgram();
+
+        booking1.rearrangeSongOrder(1, 2);
+        booking1.printSongProgram();
+
+    }
+}
