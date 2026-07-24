3b024b58872c0744ab1084dd35c54a166cce8b03
Daniel <daniel.lukins1@gmail.com>
2026-07-24 11:33:11 +1000

    added serching based on usertype

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,15 +9,45 @@ class Main {
   static final int ROW_COUNT = 10;
   static final int COLUMN_COUNT = 10;
   static final int NUM_OF_DAYS = 31;
-  private List<Booking> bookings;
+  private List<Person> registeredUsers;
   private Map<Integer, Booking> dateToBooking;
 
   Main() {
+
+    this.registeredUsers = new ArrayList<>();
+
     // initialise dateToBooking with 31 empty slots
     this.dateToBooking = new HashMap<>();
     for (int i = 1; i < NUM_OF_DAYS + 1; i++) {
       this.dateToBooking.put(i, null);
     }
+  }
+
+  public void addUser(Person person) {
+    this.registeredUsers.add(person);
+  }
+
+  public void displayUsers() {
+    for (Person user : this.registeredUsers) {
+      System.out.println(user);
+    }
+  }
+
+  public void displayArtists() {
+    for (Person user : this.registeredUsers) {
+      if (user instanceof Artist) {
+        System.out.println((Artist) user);
+      }
+    }
+  }
+
+  public void displayFans() {
+    for (Person user : this.registeredUsers) {
+      if (user instanceof Fan) {
+        System.out.println((Fan) user);
+      }
+    }
+
   }
 
   // used for Making new Booking or modifying existing booking
@@ -35,6 +66,14 @@ class Main {
   public void removeBooking(Booking booking) {
     if (this.dateToBooking.containsKey(booking.getDate())) {
       this.dateToBooking.put(booking.getDate(), null);
+    }
+  }
+
+  public void displayBookings() {
+    for (Map.Entry<Integer, Booking> e : this.dateToBooking.entrySet()) {
+      if (e.getValue() != null) {
+        System.out.println(e.getValue() + ", " + "MONTH," + e.getKey());
+      }
     }
   }
 
